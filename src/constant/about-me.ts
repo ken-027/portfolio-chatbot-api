2a35518b024794d2cb9e2488e914df1b8d8dbe0e
ken <keanolida7296@gmail.com>
2025-07-12 23:44:08 +0800

    feat(info): adjust resume, experience descriptions, and info

--- src/constant/about-me.ts
+++ src/constant/about-me.ts
@@ -1,7 +1,8 @@
 const ABOUT_ME: string[] = [
-    "Hi there! I'm Kenneth Andales, a passionate <strong>Software Developer</strong> with years of experience crafting high-performance applications. I love building intuitive user experiences and writing clean, efficient code that delivers real results.",
-    "While my core expertise lies in <strong>full-stack web development</strong>, encompassing technologies like <strong>React.js</strong>, <strong>Node.js</strong>, <strong>PHP</strong>, and <strong>SQL Server</strong>, I'm always exploring new areas to expand my craft. Currently, I'm enhancing my skills with certifications in <strong>LLM Engineering</strong>, <strong>Agentic AI Engineering</strong>, <strong>Python</strong>, and <strong>DevOps</strong>, ensuring I'm on top of the latest industry trends.",
-    "I've had the opportunity to work on diverse projects, from core banking systems, government applications, and e-commerce platforms to AI-powered applications. My goal is always to deliver reliable, results-oriented solutions that make a difference. Let's build something amazing together!",
+    "Hi there! I’m Kenneth Andales, a dedicated <strong>Software Developer</strong> with over 3 years of experience building user-focused and reliable applications.",
+    "My expertise is in full‑stack web development using <strong>Tailwind CSS</strong>, <strong>React.js</strong>, <strong>Node.js</strong>, <strong>PHP</strong> and <strong>SQL</strong>. with experience in government portals, e-commerce sites, and cooperative finance/core banking systems like NATCCO's eKoopBanker.",
+    "Currently, I’m expanding my skills with certifications in <strong>LLM Engineering</strong>, <strong>Agentic AI Engineering</strong>, <strong>Python</strong>, and <strong>DevOps</strong>—staying ahead of emerging trends.",
+    "Known for reliable delivery, clean code, and seamless collaboration, I partner with teams to ship faster while ensuring performance, scalability, and real-world impact. Let’s build something great!",
 ];
 
 export default ABOUT_ME;
